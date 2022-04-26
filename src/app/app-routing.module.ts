88a8b4957c80b9e3227f3e01e9e52ea9ea3f4964
Thaïs <thais.r@live.fr>
2022-04-26 10:51:18 +0200

    :sparkles: présentation syntaxe de template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PresentationComponent} from "./pages/presentation/presentation.component";
+import {CommunicationComponent} from "./pages/communication/communication.component";
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'accueil', component: PresentationComponent},
+  {path: 'communication', component: CommunicationComponent},
+  {path: 'acceuil', redirectTo: 'accueil'},
+  {path: '', redirectTo: '/accueil', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresentationComponent } from './pages/presentation/presentation.component';
+import { CommunicationComponent } from './pages/communication/communication.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PresentationComponent
+    PresentationComponent,
+    CommunicationComponent,
+    PageNotFoundComponent,
+    NavigationComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
